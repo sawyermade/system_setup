b10ab983070455dee960f29a90247c40b9e9f20d
sawyermade <sawyermade0@gmail.com>
2021-07-09 12:39:43 -0400

    oc

--- aliases.sh
+++ aliases.sh
@@ -14,6 +14,7 @@ alias py='python3'
 alias gpr='git pull --recurse-submodules'
 alias rsync-app='rsync -rzvP --append --inplace'
 alias xming="export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0"
+alias oc='sudo openconnect --protocol=gp vpn.usf.edu'
 ### ALIAS ###
 
 ### SSH ALIAS ###
